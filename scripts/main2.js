c68e26789ac2715c9526c0fd0321b7fd82a56745
robert <robert07spiridon@gmail.com>
2023-03-04 14:16:14 +0200

    links on both pages

--- scripts/main2.js
+++ scripts/main2.js
@@ -65,4 +65,16 @@ const speakers=document.getElementById('speakers');
 speakers.setAttribute('href', './page2.html?category=Speakers');
 const earphones=document.getElementById('earphones');
 earphones.setAttribute('href', './page2.html?category=Earphones');
+const catHeadphones=document.getElementById('CatHeadphones');
+catHeadphones.addEventListener('click',()=>{
+    location.replace("page2.html?category=Headphones")
+})
+const catSpeakers=document.getElementById('CatSpeakers');
+catSpeakers.addEventListener('click',()=>{
+    location.replace("page2.html?category=Speakers")
+})
+const catEarphones=document.getElementById('CatEarphones');
+catEarphones.addEventListener('click',()=>{
+    location.replace("page2.html?category=Earphones")
+})
 })
